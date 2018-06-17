24309ee0eb7a94e35ce8e6ed6a8a6dfbc82806fe
monaazami <32145698+monaazami@users.noreply.github.com>
2018-06-17 12:20:19 +0100

    created sales app with react

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Coursesales from "./course-sales.js";
 import './App.css';
 
 class App extends Component {
   render() {
+    var courses =[
+      {name:"Graphic design" ,price:49},
+      {name:"Web design" ,price:60},
+      {name:"Illustration" ,price:35},
+      {name:"Photography" ,price:65}
+    ]
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Course sales React app!</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+        <Coursesales items={courses}/>
       </div>
     );
   }
---
+++ src/course-sales.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+
+class Coursesales extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			total: 0
+        };
+        this.sumPrice=this.sumPrice.bind(this);
+    }
+    
+    sumPrice(price){
+        this.setState({total:this.state.total+price })
+    }
+	render() {
+        var courses=this.props.items.map((item,i)=> {
+            return <Course name={item.name} price={item.price} 
+            key={i} sumPrice={this.sumPrice} active ={item.active}/>
+        });
+		return (
+			<div>
+				<h1>You can buy courses:</h1>
+				<div id="courses">
+                    {courses}
+                    <p id="total">Total <b>{this.state.total}</b></p>
+                </div>
+			</div>
+		);
+	}
+}
+export default Coursesales;
+
+class Course extends Component {
+    constructor(props){
+        super(props);
+        this.state={ 
+            active:false
+        };
+        this.clicker=this.clicker.bind(this);
+    }
+    clicker(){
+        var active =!this.state.active;
+        this.setState({active:active});
+        this.props.sumPrice(active ? this.props.price : -this.props.price);
+    }
+    render(){
+        return(
+            <div>
+                <p onClick={this.clicker}>{this.props.name} <b>{this.props.price}</b></p>
+            </div>
+        );
+    }
+}

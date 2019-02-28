6f3b671ca252d04c62497a61aee3217982e4e677
monaazami <32145698+monaazami@users.noreply.github.com>
2019-02-28 16:50:56 +0000

    init the project

--- src/App.js
+++ src/App.js
@@ -1,30 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import Coursesales from "./course-sales.js";
-import TicTac from "./TicTac";
-import Gallery from "./Gallery";
 import './App.css';
 
 class App extends Component {
-  render() {
-    var courses =[
-      {name:"Graphic design" ,price:49},
-      {name:"Web design" ,price:60},
-      {name:"Illustration" ,price:35},
-      {name:"Photography" ,price:65}
-    ]
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Course sales React app!</h1>
-        </header> 
-        <Coursesales items={courses}/> 
-        <Gallery />
-        <TicTac />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<h1>Welcome to my app!</h1>
+			</div>
+		);
+	}
 }
 
 export default App;
